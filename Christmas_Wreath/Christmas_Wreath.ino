7e7a215982339d523da94c1285577500a0228b67
DrFoz <tfosdick@pdx.edu>
2019-12-20 00:09:53 -0800

    Update Christmas_Wreath.ino
    update

--- Christmas_Wreath/Christmas_Wreath.ino
+++ Christmas_Wreath/Christmas_Wreath.ino
@@ -21,7 +21,7 @@
 //   NEO_KHZ400  400 KHz (classic 'v1' (not v2) FLORA pixels, WS2811 drivers)
 //   NEO_GRB     Pixels are wired for GRB bitstream (most NeoPixel products)
 //   NEO_RGB     Pixels are wired for RGB bitstream (v1 FLORA pixels, not v2)
-Adafruit_NeoPixel strip = Adafruit_NeoPixel(17, PIN, NEO_RGB + NEO_KHZ400);
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(17, PIN, NEO_RGB + NEO_KHZ800);
 
 // IMPORTANT: To reduce NeoPixel burnout risk, add 1000 uF capacitor across
 // pixel power leads, add 300 - 500 Ohm resistor on first pixel's data input
@@ -30,9 +30,7 @@ Adafruit_NeoPixel strip = Adafruit_NeoPixel(17, PIN, NEO_RGB + NEO_KHZ400);
 
 void setup() {
   // This is for Trinket 5V 16MHz, you can remove these three lines if you are not using a Trinket
-  #if defined (__AVR_ATtiny85__)
-    if (F_CPU == 16000000) clock_prescale_set(clock_div_1);
-  #endif
+  
   // End of trinket special code
 
 
